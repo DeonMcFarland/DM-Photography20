7974431dd69295b7f12e1c8ca11f23769bf0cac0
DeonMcFarland <111295015+DeonMcFarland@users.noreply.github.com>
2022-09-04 17:14:56 -0400

    added a button

---
+++ myjava.js
@@ -0,0 +1,25 @@
+myitem = document.getElementById("firsttest");
+
+    myitem.addEventListener("click", onClick);
+    myitem.style.fontsize="36px";
+myitem.style.color= "blue";
+thebutton = document.getElementById("thebutton"); 
+thebutton.addEventListener("click", onButtonClick);
+otheritem = document.getElementById("buttontest");  
+
+
+
+function onClick() {  
+        myitem.style.color = "green";  
+    }
+    function onButtonClick() {  
+        otheritem.style.color = "yellow";  
+    }
+
+    textentry = document.getElementById("myinput");  
+textentry.addEventListener("change", onChange);
+
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
+}
