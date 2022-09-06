b05064d51ba6fdbf0539be8a4baf87ce97ab1e1f
DeonMcFarland <111295015+DeonMcFarland@users.noreply.github.com>
2022-09-06 11:40:00 -0400

    delete

--- myjava.js
+++ myjava.js
@@ -24,11 +24,3 @@ function onChange() {
     otheritem.innerHTML = newtext;  
 }
 
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
